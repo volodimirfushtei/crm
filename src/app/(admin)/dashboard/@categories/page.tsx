164289759614c95ec120c39f8a158bad9981cc4a
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2025-05-27 00:16:54 +0300

    add map

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import {getCategories, getCompanies} from '@/app/lib/api';
+import { getCategories, getCompanies } from '@/app/lib/api';
 
 
 import getCountById from '@/app/lib/utils/getCountById';
-import StatCard, {StatCardType} from '@/app/components/stat-card/stat-card';
+import StatCard, { StatCardType } from '@/app/components/stat-card/stat-card';
 import DashboardCard from '@/app/components/dashboard-card';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+
 export interface PageProps {
 
 }
 
 export default async function Page({}: PageProps) {
-    const categories = await getCategories();
-    const companies = await getCompanies();
-    const counts = getCountById(companies, 'categoryId')
+  const categories = await getCategories();
+  const companies = await getCompanies();
+  const counts = getCountById(companies, 'categoryId');
 
-    return (
-        <DashboardCard label="Categories of companies">
-            <div className="grid grid-cols-12 gap-3 pb-5 px-5 cursor-pointer drop-shadow-xl ">
-                {categories.map(({id, title}) => (
+  return (
+    <DashboardCard label="Categories of companies">
+      <div className="grid grid-cols-12 gap-3 pb-5 px-5 cursor-pointer drop-shadow-xl ">
+        {categories.map(({ id, title }) => (
 
 
-                    <div key={id} className="col-span-3 ">
-                        <StatCard
-                            type={StatCardType.Dark}
-                            label={title}
-                            counter={counts[id] || 0}
-                        />
-                    </div>
-                ))}
-            </div>
-        </DashboardCard>
-    );
+          <div key={id} className="col-span-3 ">
+            <StatCard
+              type={StatCardType.Dark}
+              label={title}
+              counter={counts[id] || 0}
+            />
+          </div>
+        ))}
+      </div>
+    </DashboardCard>
+  );
 }
