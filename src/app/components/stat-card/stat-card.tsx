3602007071dc6b33fb85fb7fa87bde7725ca4f8d
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2025-05-27 00:50:30 +0300

    fix css

--- src/app/components/stat-card/stat-card.tsx
+++ src/app/components/stat-card/stat-card.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import clsx from 'clsx';
-import styles from './styles.module.css';
+import styles from './stat-card.module.css';
 
 export enum StatCardType {
-    Dark = 'dark',
-    Gradient = 'gradient',
+  Dark = 'dark',
+  Gradient = 'gradient',
 }
 
 export interface StatCardProps {
-    type: StatCardType;
-    label: string;
-    counter: number;
+  type: StatCardType;
+  label: string;
+  counter: number;
 }
 
-export default function StatCard({type, label, counter}: StatCardProps) {
-    return (
-        <div
-            className={clsx(
-                'rounded  bg-black-10',
-                type === StatCardType.Dark &&
-                'pt-3 px-3 bg-gray-900 odd:text-purple-200 even:text-lime-200 text-right ',
-                type === StatCardType.Gradient && [
-                    styles.gradient,
-                    'p-7 bg-purple-200 text-gray-900 text-left',
-                ],
-            )}
-        >
-            <p
-                className={clsx(
-                    'text-left before:w-4 before:h-0.5 before:rounded',
-                    type === StatCardType.Dark &&
-                    'text-sm mb-1 text-zinc-50 before:block before:mb-2 before:bg-zinc-50',
-                    type === StatCardType.Gradient &&
-                    'text-xs mb-5 before:inline-block before:mr-2 before:align-middle before:bg-gray-900',
-                )}
-            >
-                {label}
-            </p>
-            <p className="text-6xl font-semibold">{counter}</p>
-        </div>
-    );
+export default function StatCard({ type, label, counter }: StatCardProps) {
+  return (
+    <div
+      className={clsx(
+        'rounded  bg-black-10',
+        type === StatCardType.Dark &&
+        'pt-3 px-3 bg-gray-900 odd:text-purple-200 even:text-lime-200 text-right ',
+        type === StatCardType.Gradient && [
+          styles.gradient,
+          'p-7 bg-purple-200 text-gray-900 text-left',
+        ],
+      )}
+    >
+      <p
+        className={clsx(
+          'text-left before:w-4 before:h-0.5 before:rounded',
+          type === StatCardType.Dark &&
+          'text-sm mb-1 text-zinc-50 before:block before:mb-2 before:bg-zinc-50',
+          type === StatCardType.Gradient &&
+          'text-xs mb-5 before:inline-block before:mr-2 before:align-middle before:bg-gray-900',
+        )}
+      >
+        {label}
+      </p>
+      <p className="text-6xl font-semibold">{counter}</p>
+    </div>
+  );
 }
--- src/app/lib/utils/getCountById.ts
+++ src/app/lib/utils/getCountById.ts
@@ -1,20 +1,18 @@
 const getCountById = <T extends string, I extends Record<T, string>>(
-    items: I[],
-    idKey: T,
+  items: I[],
+  idKey: T,
 ) =>
+  items.reduce(
+    (counts, { [idKey]: id }) => {
 
-    items.reduce(
-        (counts, {[idKey]: id}) => {
+      if (!counts[id]) counts[id] = 0;
 
-            if (!counts[id]) counts[id] = 0;
+      counts[id] += 1;
+      return counts;
 
-            counts[id] += 1;
-            return counts;
+    },
 
-        },
-
-        {} as Record<string, number>,
-    );
-
+    {} as Record<string, number>,
+  );
 
 export default getCountById;
